4c26a4c8013eb710f68058de7c028245d862ec82
Eliah7 <eliahmbwilo7@gmail.com>
2021-02-18 14:27:11 +0300

    ${1}

--- functions/apis/service-discovery.api.mjs
+++ functions/apis/service-discovery.api.mjs
@@ -1,12 +1,12 @@
 import bfastnode from "bfastnode";
 const {bfast, BFast} = bfastnode;
 
-export const serviceDiscoveryApi = BFast.functions().onHttpRequest('/.well-known/tz-UTS-server', (request, response) => {
+export const serviceDiscoveryApi = BFast.functions().onHttpRequest('/.well-known/tz-e-ticketing-server', (request, response) => {
     const hostPath = "https" + '://' + request.get('host') ;
     const result = {
-        journeys_update_endpoint_url: `${hostPath}/journey/update`,
-        search_endpoint_url: `${hostPath}/journey/search`,
-        jwks_uri: `${hostPath}/jwks`
+        reserve_endpoint_url: `${hostPath}/journey/reserve`,
+        jwks_uri: `${hostPath}/jwks`,
+        vendor_name: `BUS POA`
     }
 
     response.json(result);
