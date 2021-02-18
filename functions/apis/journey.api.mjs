d9bb398c33e8bf83b95dd8bdb5c814bef62e193f
Eliah7 <eliahmbwilo7@gmail.com>
2021-02-18 13:52:30 +0300

    {$1}

--- functions/apis/journey.api.mjs
+++ functions/apis/journey.api.mjs
@@ -1,25 +1,41 @@
 import bfastnode from "bfastnode";
+import {JourneyService} from "../services/journey.service.mjs";
 
 const {bfast, BFast} = bfastnode;
+const journeyService = new JourneyService();
 
 function validateJourneyList(body) {
     return (
         body && Array.isArray(body) &&
         body.every((journey) => {
             return (
-                journey["journey_id"] &&
-                journey["journey_endpoint_url"] &&
-                journey["company"] &&
-                journey["trip_id"] &&
-                journey["bus_plate_number"] &&
-                journey["origin"] &&
-                journey["seats"] &&
-                journey["pickup_up_points"] &&
-                journey["departure_date"] &&
-                journey["departure_time"] &&
-                journey["arrival_date"] &&
-                journey["arrival_time"]
-            )
+                journey["journey_id"]
+                // &&
+                // journey["journey_endpoint_url"] &&
+                // journey["company_name"] &&
+                // journey["brand_name"] &&
+                // journey["schedule_id"] &&
+                // journey["schedule_name"] &&
+                // journey["schedule_bus_plate_number"] &&
+                // journey["amenities"] &&
+                // journey["class"] &&
+                // journey["origin"] &&
+                // journey["destination"] &&
+                // journey["destination_search_token"] &&
+                // journey["origin_search_tokens"] &&
+                // journey["trip_id"] &&
+                // journey["bus_plate_number"] &&
+                // journey["origin"] &&
+                // journey["seats"] &&
+                // journey["price"] &&
+                // journey["pickup_up_points"] &&
+                // journey["drop_off_points"] &&
+                // journey["departure_date"] &&
+                // journey["departure_time"] &&
+                // journey["arrival_date"] &&
+                // journey["arrival_time"] &&
+                // journey["last_modified"]
+        )
         }))
 }
 
@@ -29,10 +45,28 @@ export const updateJourneyApi = BFast.functions().onPostHttpRequest(
         const body = request.body;
 
         if (validateJourneyList(body)) {
+            const journeys = body.map(journey => {
+                return {
+                    id: journey['journey_id'],
+                    ...journey
+                }
+            });
 
+            journeyService.saveJourneys(journeys).then(
+                result => {
+                    response.status(200).json(result);
+                }
+
+            ).catch(err => {
+                console.warn({message: err})
+                response.status(404).json({message: "Failed to save journies"})
+            });
+        } else{
+
+            response.status(404).json({message: "Failed to save journies"})
         }
 
-        response.json([]);
+
     });
 
 export const searchJourneyApi = BFast.functions().onHttpRequest('/journey/search', (request, response) => {
---
+++ functions/services/journey.service.mjs
@@ -0,0 +1,29 @@
+import bfastnode from "bfastnode";
+
+const {bfast} = bfastnode;
+
+export class JourneyService {
+    static COLLECTION_NAME = 'journeys';
+
+    /**
+     *
+     * @param journeys: []
+     * @returns {Promise<any>}
+     */
+    async saveJourneys(journeys) {
+        return Promise.all(journeys.map(async journey => {
+           return bfast.database().collection(JourneyService.COLLECTION_NAME).get(journey.id).then( j => {
+               if (j) {
+                   return bfast.database().collection(JourneyService.COLLECTION_NAME).query().updateBuilder().doc(journey).update();
+               } else {
+                   return bfast.database().collection(JourneyService.COLLECTION_NAME).save(journey);
+               }
+           }).catch( err => {
+               console.warn(err)
+               return err;
+           });
+
+        }));
+
+    }
+}
