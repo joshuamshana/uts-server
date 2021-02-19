e8218c2e53c31caa0857d502e1d67bf00dec4c28
Eliah7 <eliahmbwilo7@gmail.com>
2021-02-19 14:37:54 +0300

    reserve_journey

--- functions/services/journey.service.mjs
+++ functions/services/journey.service.mjs
@@ -16,7 +16,7 @@ export class JourneyService {
      */
     async saveJourneys(journeys) {
         return Promise.all(journeys.map(async journey => {
-           return bfast.database().collection(JourneyService.COLLECTION_NAME).get(journey.id).then( j => {
+           return bfast.database().collection(JourneyService.COLLECTION_NAME).get(journey.id).then( async j => {
                if (j) {
                    return await bfast.database().collection(JourneyService.COLLECTION_NAME).query().updateBuilder().doc(journey).update();
                } else {
@@ -25,32 +25,32 @@ export class JourneyService {
            }).catch();
 
         }));
-
     }
 
-
     async reserveJourney(reserve) {
+        const busPoaUrlReserve = "https://www.buspoa.co.tz/manifest/utsrequest_poa.php";
         if (this._validateReserveData(reserve)) {
-            // todo: implement reserve from bus poa
             console.log(reserve);
-            return {
-                "type": "mock",
-                "reservation_id": reserve.reservation_request_id,
-                "reservation_request_id": RandomUtil.uuid,
-                "reservation_url": "https://",
-                "journey_id": "xyz123",
-                "expires": moment().format("YYYY-MM-DDThh:mm:ssZ"),
-                "passengers:": [
-                    {
-                        "passenger_index": 0,
-                        "first_name": "John",
-                        "middle_name": "",
-                        "last_name": "Doe",
-                        "seat": "A1",
-                        "price": "12500.00"
-                    }
-                ],
-            }
+            return bfast.functions().request(busPoaUrlReserve).post(reserve);
+
+            // return {
+            //     "type": "mock",
+            //     "reservation_id": reserve.reservation_request_id,
+            //     "reservation_request_id": RandomUtil.uuid,
+            //     "reservation_url": "https://",
+            //     "journey_id": "xyz123",
+            //     "expires": moment().format("YYYY-MM-DDThh:mm:ssZ"),
+            //     "passengers:": [
+            //         {
+            //             "passenger_index": 0,
+            //             "first_name": "John",
+            //             "middle_name": "",
+            //             "last_name": "Doe",
+            //             "seat": "A1",
+            //             "price": "12500.00"
+            //         }
+            //     ],
+            // }
         } else {
             throw {message: 'invalid data supplied'};
         }
@@ -64,11 +64,10 @@ export class JourneyService {
     _validateReserveData(data) {
         return !!(data &&
             data.reservation_request_id &&
-            data.journey_id &&
-            // data.passengers &&
-            data.bording_point_id &&
-            data.bording_point_name &&
-            data.dropoff_point_id &&
-            data.dropoff_point_name);
+            data.journey_id )
+            // &&
+            // (data.passengers && Array.isArray(data.passengers) && data.passengers.length > 0) &&
+            // data.language &&
+            // data.phone_number) ;
     }
 }
