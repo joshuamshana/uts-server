02a40e834a0eb3e0f87d9171c185c4464c9d5292
joshua mshana <mama27j@gmail.com>
2021-02-22 10:43:11 +0300

    update reserve journey

--- functions/services/journey.service.mjs
+++ functions/services/journey.service.mjs
@@ -1,8 +1,5 @@
 import bfastnode from "bfastnode";
 
-import moment from "moment";
-import {RandomUtil} from "../utils/random.util.mjs";
-
 
 const {bfast} = bfastnode;
 
@@ -16,23 +13,24 @@ export class JourneyService {
      */
     async saveJourneys(journeys) {
         return Promise.all(journeys.map(async journey => {
-           return bfast.database().collection(JourneyService.COLLECTION_NAME).get(journey.id).then( async j => {
-               if (j) {
-                   return await bfast.database().collection(JourneyService.COLLECTION_NAME).query().updateBuilder().doc(journey).update();
-               } else {
-                   return await bfast.database().collection(JourneyService.COLLECTION_NAME).save(journey);
-               }
-           }).catch();
+            return bfast.database().collection(JourneyService.COLLECTION_NAME).get(journey.id).then(async j => {
+                if (j) {
+                    return await bfast.database().collection(JourneyService.COLLECTION_NAME).query().updateBuilder().doc(journey).update();
+                } else {
+                    return await bfast.database().collection(JourneyService.COLLECTION_NAME).save(journey);
+                }
+            }).catch();
 
         }));
     }
 
     async reserveJourney(reserve) {
         const busPoaUrlReserve = "https://www.buspoa.co.tz/manifest/utsrequest_poa.php";
+        console.log(reserve);
         if (this._validateReserveData(reserve)) {
-            console.log(reserve);
-            return bfast.functions().request(busPoaUrlReserve).post(reserve);
-
+            const results = await bfast.functions().request(busPoaUrlReserve).post(reserve);
+            console.log(results);
+            return results;
             // return {
             //     "type": "mock",
             //     "reservation_id": reserve.reservation_request_id,
@@ -62,12 +60,13 @@ export class JourneyService {
      * @param data {*}
      */
     _validateReserveData(data) {
-        return !!(data &&
-            data.reservation_request_id &&
-            data.journey_id )
-            // &&
-            // (data.passengers && Array.isArray(data.passengers) && data.passengers.length > 0) &&
-            // data.language &&
-            // data.phone_number) ;
+        return true;
+        // return !!(data &&
+        //     data.reservation_request_id &&
+        //     data.journey_id )
+        // &&
+        // (data.passengers && Array.isArray(data.passengers) && data.passengers.length > 0) &&
+        // data.language &&
+        // data.phone_number) ;
     }
 }
